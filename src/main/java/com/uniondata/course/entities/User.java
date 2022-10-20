5a40862dd98ff0c5332ffda5343bffaa3ad10494
Michael Bernard <mich4el_bernard@hotmail.com>
2022-10-20 15:13:45 -0300

    H2 database, test profile, JPA

--- src/main/java/com/uniondata/course/entities/User.java
+++ src/main/java/com/uniondata/course/entities/User.java
@@ -3,10 +3,20 @@ package com.uniondata.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
